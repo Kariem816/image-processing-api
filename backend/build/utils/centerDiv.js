379cd87aacbdb37b806e734ca1118ad27514c98d
Bumble <63314839+Kariem816@users.noreply.github.com>
2022-11-11 01:39:05 +0200

    removed build files

--- backend/build/utils/centerDiv.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function centerDiv(text) {
-    return "<div\n            style=\"\n                height:100%;\n                width: 100%;\n                display: grid;\n                place-items: center;\n                text-align: center;\n            \"\n        >\n            ".concat(text, "\n        </div>");
-}
-exports.default = centerDiv;
